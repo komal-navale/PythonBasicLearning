3ab41f8a125bfaf3ba170a146ce5e9e9ccde5b4b
Komal Navale <komalniteshnavale@outlook.com>
2021-08-12 13:41:57 +0530

    First Commit

---
+++ Python Indentation & Comments/Indentation&Comments.py
@@ -0,0 +1,10 @@
+if 10 > 5:
+    print('10 is greater than 5')
+
+
+# This is a comment
+print('Comments Demo')  # This is a in-line comment
+
+# Multi-line comments
+# There is nothing like multi-line comments in Python like other programming languages.
+# To display comments it has to start with # in front of each line
